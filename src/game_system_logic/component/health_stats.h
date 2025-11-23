f8bf9a8e1855e3d5aa879d1563b8315dd838d380
TimoGuy <timoguy99004@gmail.com>
2025-11-22 20:21:00 -0800

    Tunes attack receive debounce time.

--- src/game_system_logic/component/health_stats.h
+++ src/game_system_logic/component/health_stats.h
@@ -22,7 +22,7 @@ struct Health_stats_data
 
     bool is_invincible{ false };           // `true` prevents death trigger and decrement of `health_pts`.
 
-    double_t atk_receive_debounce_time{ 0.1 };  // Min time between attacks in seconds.
+    double_t atk_receive_debounce_time{ 0.2 };  // Min time between attacks in seconds.
     double_t prev_atk_received_time{ std::numeric_limits<double_t>::lowest() };  // DO NOT INCLUDE IN SERIALIZATION.
 
     NLOHMANN_DEFINE_TYPE_INTRUSIVE_WITH_DEFAULT(
