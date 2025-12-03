ac0ddfc0e74bf0173f89c0c7b668ed183ee51b2a
TimoGuy <timoguy99004@gmail.com>
2025-12-02 21:56:52 -0800

    Creates script to update cmakelists file

--- cmakelists_update.py
+++ cmakelists_update.py
@@ -1,33 +1,8 @@
 from pathlib import Path
 
-
-def find_src_entries() -> list[str]:
-    CMAKELISTS_FNAME = './CMakeLists.txt'
-    START_BLOCK = 'set(MAIN_SOURCES'
-    END_BLOCK = ')'
-    SRC_FILE_ENTRY_START = '${CMAKE_CURRENT_SOURCE_DIR}'
-
-    block_process = 0  # 0:before; 1:within; 2:after;
-    found_source_files = []
-    with open(CMAKELISTS_FNAME) as f:
-        for line in f:
-            if block_process == 0:
-                if line.strip() == START_BLOCK:
-                    # Found start of block.
-                    block_process = 1
-            elif block_process == 1:
-                if line.strip().startswith(SRC_FILE_ENTRY_START):
-                    # Add file entry.
-                    entry_start_str_len = len(SRC_FILE_ENTRY_START)+1  # +1 for '/' after.
-                    found_source_files.append(line.strip()[entry_start_str_len:])
-                elif line.strip() == END_BLOCK:
-                    # Found end of block.
-                    block_process = 2
-            elif block_process == 2:
-                # Exit reading file.
-                break
-
-    return found_source_files
+CMAKELISTS_FNAME = './CMakeLists.txt'
+START_BLOCK = 'set(MAIN_SOURCES'
+END_BLOCK = ')'
 
 
 def find_existing_files() -> list[Path]:
@@ -53,29 +28,48 @@ def find_existing_files() -> list[Path]:
     return all_found_files
 
 
-def find_missing_src_entry_in_src_entries(src_entries: list[str],
-                                          existing_files: list[Path]):
-    missing_entries = []  # If the file exists but not the entry in cmake, then append!
-    src_entry_paths = [Path(x) for x in src_entries]
-    for existing_file in existing_files:
-        if existing_file not in src_entry_paths:
-            # Found missing entry.
-            missing_entry = str(existing_file).replace('\\', '/')
-            missing_entries.append(missing_entry)
+def prepend_src_file_entry(fname: Path) -> str:
+    return '    ${CMAKE_CURRENT_SOURCE_DIR}/' + f'{fname.as_posix()}'
 
-    missing_entries.sort()
-    return missing_entries
+
+def read_existing_surrounding_src_entries_strings() -> tuple[str, str]:
+    before_str_block = ''
+    after_str_block = ''
+
+    block_process = 0  # 0:before; 1:within; 2:after;
+    found_source_files = []
+    with open(CMAKELISTS_FNAME, 'r') as f:
+        for line in f:
+            if block_process == 0:
+                # Add before str line.
+                before_str_block += line
+
+                if line.strip() == START_BLOCK:
+                    # Found start of block.
+                    block_process = 1
+            elif block_process == 1:
+                if line.strip() == END_BLOCK:
+                    # Add after str line.
+                    after_str_block += line
+
+                    # Found end of block.
+                    block_process = 2
+            elif block_process == 2:
+                # Add after str line.
+                after_str_block += line
+
+    return before_str_block, after_str_block
 
 
 if __name__ == '__main__':
-    src_entries = find_src_entries()
     existing_files = find_existing_files()
+    src_entries = [prepend_src_file_entry(x) for x in existing_files]
+    src_entries.sort()
+    src_entries_str_block = '\n'.join(src_entries) + '\n'  # Add newline at end of block.
 
-    files_missing_in_src_entries = \
-        find_missing_src_entry_in_src_entries(src_entries, existing_files)
+    before_str_block, after_str_block = read_existing_surrounding_src_entries_strings()
 
-    # Print result.
-    if len(files_missing_in_src_entries) > 0:
-        print('==== MISSING ENTRIES ============================================')
-    for missing_file in files_missing_in_src_entries:
-        print(missing_file)
+    complete_str_data = before_str_block + src_entries_str_block + after_str_block
+    
+    with open(CMAKELISTS_FNAME, 'w') as f:
+        f.write(complete_str_data)
